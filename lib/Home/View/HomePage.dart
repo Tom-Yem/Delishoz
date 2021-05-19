de50afc3a9f053e1cbdeb6cf08cf6338af6a0098
Tom <Tomy51451@gmail.com>
2021-05-19 13:40:15 +0200

    :sparkles: Add home view

--- lib/Home/View/HomePage.dart
+++ lib/Home/View/HomePage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+// core modules
 import 'package:recipe_app/Search/Controller/recipeController.dart';
 import 'package:recipe_app/Search/View/Recipes.dart';
 
@@ -13,34 +15,78 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("$title")),
-      body: Center(
-        child: Container(
-          width: 500,
-          height: 500,
-          // decoration: BoxDecoration(border: Border.all(color: Colors.red)),
-          child: Column(
-            children: [
-              Text("Search for your items here"),
-              TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Search",
+        appBar: AppBar(title: Text("$title")),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("/home_wallpaper.jpg"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            Container(
+              color: Colors.black.withOpacity(0.6),
+            ),
+            Center(
+              child: Container(
+                width: 500,
+                height: 500,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Search it, Make it, \nEat it!",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3
+                          ?.copyWith(color: Colors.white),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            style: TextStyle(color: Colors.white),
+                            controller: _controller,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white)),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.white)),
+                                labelText: "Search",
+                                // filled: true,
+                                // fillColor: Colors.white30,
+                                labelStyle: TextStyle(color: Colors.black26)),
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                              padding: MaterialStateProperty.all(
+                                  EdgeInsets.symmetric(vertical: 18))),
+                          onPressed: () {
+                            recipeController.searchMeal(_controller.text);
+                            // Get.toNamed("/search");
+                            Get.to(RecipesPage());
+                          },
+                          child: Icon(
+                            Icons.search,
+                            size: 32,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  recipeController.searchMeal(_controller.text);
-                  // Get.toNamed("/search");
-                  Get.to(RecipesPage());
-                },
-                child: Text("search"),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          ],
+        ));
   }
 }
