d88f8648e6a9a7165720180c78664edfd1617fa4
Tom <Tomy51451@gmail.com>
2021-05-19 13:48:58 +0200

    Remove debug banner from screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+// Homepage view
 import 'package:recipe_app/Home/View/HomePage.dart';
 
 void main() {
@@ -12,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
