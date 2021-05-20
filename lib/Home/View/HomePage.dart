4fb0d98adf338e0882fc7053dfa3df2eda372fba
Tom <Tomy51451@gmail.com>
2021-05-20 10:28:19 +0200

    :sparkles: Make search input field unfocus  when clicking on screen body

--- lib/Home/View/HomePage.dart
+++ lib/Home/View/HomePage.dart
@@ -14,79 +14,83 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("$title")),
-        body: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("/home_wallpaper.jpg"),
-                    fit: BoxFit.cover),
-              ),
-            ),
-            Container(
-              color: Colors.black.withOpacity(0.6),
-            ),
-            Center(
-              child: Container(
-                width: 500,
-                height: 500,
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Search it, Make it, \nEat it!",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline3
-                          ?.copyWith(color: Colors.white),
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            style: TextStyle(color: Colors.white),
-                            controller: _controller,
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.white)),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.white)),
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.white)),
-                                labelText: "Search",
-                                // filled: true,
-                                // fillColor: Colors.white30,
-                                labelStyle: TextStyle(color: Colors.black26)),
-                          ),
-                        ),
-                        ElevatedButton(
-                          style: ButtonStyle(
-                              padding: MaterialStateProperty.all(
-                                  EdgeInsets.symmetric(vertical: 18))),
-                          onPressed: () {
-                            recipeController.searchMeal(_controller.text);
-                            // Get.toNamed("/search");
-                            Get.to(RecipesPage());
-                          },
-                          child: Icon(
-                            Icons.search,
-                            size: 32,
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+          appBar: AppBar(title: Text("$title")),
+          body: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("/home_wallpaper.jpg"),
+                      fit: BoxFit.cover),
                 ),
               ),
-            ),
-          ],
-        ));
+              Container(
+                color: Colors.black.withOpacity(0.6),
+              ),
+              Center(
+                child: Container(
+                  width: 500,
+                  height: 500,
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Search it, Make it, \nEat it!",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline3
+                            ?.copyWith(color: Colors.white),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              cursorColor: Colors.white,
+                              style: TextStyle(color: Colors.white),
+                              controller: _controller,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.white)),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.white)),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.white)),
+                                  labelText: "Search",
+                                  // filled: true,
+                                  // fillColor: Colors.black3,
+                                  labelStyle: TextStyle(color: Colors.white)),
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ButtonStyle(
+                                padding: MaterialStateProperty.all(
+                                    EdgeInsets.symmetric(vertical: 18))),
+                            onPressed: () {
+                              recipeController.searchMeal(_controller.text);
+                              // Get.toNamed("/search");
+                              Get.to(RecipesPage());
+                            },
+                            child: Icon(
+                              Icons.search,
+                              size: 32,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
