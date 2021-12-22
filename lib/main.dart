d02acf0eac9b79b9b847bce900740bcd5ab6b8ee
Tom <Tomy51451@gmail.com>
2021-12-22 15:41:35 +0200

    :sparkles: NEW: Add navigation & base colors

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,20 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: MaterialColor(0xFF141f29, {
+        50: Color.fromRGBO(20, 31, 41, 1),
+        100: Color.fromRGBO(20, 31, 41, 1),
+        200: Color.fromRGBO(20, 31, 41, 1),
+        300: Color.fromRGBO(20, 31, 41, 1),
+        400: Color.fromRGBO(20, 31, 41, 1),
+        500: Color.fromRGBO(20, 31, 41, 1),
+        600: Color.fromRGBO(20, 31, 41, 1),
+        700: Color.fromRGBO(20, 31, 41, 1),
+        800: Color.fromRGBO(20, 31, 41, 1),
+        900: Color.fromRGBO(20, 31, 41, 1),
+      })),
       initialRoute: "/",
-      getPages: [GetPage(name: "/", page: () => MyHomePage(title: 'Home'))],
+      getPages: [GetPage(name: "/", page: () => MyHomePage(title: 'Delishoz'))],
     );
   }
 }
