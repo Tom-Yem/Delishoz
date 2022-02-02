81a45667582353778aaf7386054d26fe3f5758f8
Tom <Tomy51451@gmail.com>
2022-02-02 16:12:37 +0200

    :sparkles: NEW: Update font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // Homepage view
 import 'package:recipe_app/Home/View/HomePage.dart';
@@ -16,18 +17,24 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: MaterialColor(0xFF141f29, {
-        50: Color.fromRGBO(20, 31, 41, 1),
-        100: Color.fromRGBO(20, 31, 41, 1),
-        200: Color.fromRGBO(20, 31, 41, 1),
-        300: Color.fromRGBO(20, 31, 41, 1),
-        400: Color.fromRGBO(20, 31, 41, 1),
-        500: Color.fromRGBO(20, 31, 41, 1),
-        600: Color.fromRGBO(20, 31, 41, 1),
-        700: Color.fromRGBO(20, 31, 41, 1),
-        800: Color.fromRGBO(20, 31, 41, 1),
-        900: Color.fromRGBO(20, 31, 41, 1),
-      })),
+        // TODO: Clean primary swatch part
+        primarySwatch: MaterialColor(
+          0xFF141f29,
+          {
+            50: Color.fromRGBO(20, 31, 41, 1),
+            100: Color.fromRGBO(20, 31, 41, 1),
+            200: Color.fromRGBO(20, 31, 41, 1),
+            300: Color.fromRGBO(20, 31, 41, 1),
+            400: Color.fromRGBO(20, 31, 41, 1),
+            500: Color.fromRGBO(20, 31, 41, 1),
+            600: Color.fromRGBO(20, 31, 41, 1),
+            700: Color.fromRGBO(20, 31, 41, 1),
+            800: Color.fromRGBO(20, 31, 41, 1),
+            900: Color.fromRGBO(20, 31, 41, 1),
+          },
+        ),
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       initialRoute: "/",
       getPages: [GetPage(name: "/", page: () => MyHomePage(title: 'Delishoz'))],
     );
